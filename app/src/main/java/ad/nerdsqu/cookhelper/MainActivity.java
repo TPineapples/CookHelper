9e45a0acec6ac6a7967a33b6dc8d3b8d27b8f410
Talaal Mirza <t.pineapples@gmail.com>
2016-11-27 00:21:52 -0500

    Tested IsValidLogin
    Turns out it is valid... why wont it let me login :anger:

--- app/src/main/java/ad/nerdsqu/cookhelper/MainActivity.java
+++ app/src/main/java/ad/nerdsqu/cookhelper/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("x000: Attempting default login registration");
         Login temp = new Login();
         helper.addLogin(temp);
+        System.out.println(helper.IsValidLogin("Default"));
 
         final EditText username = (EditText) findViewById(R.id.fieldUsername);
         final EditText password = (EditText) findViewById(R.id.fieldPassword);
